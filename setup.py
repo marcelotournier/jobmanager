129ef02ae757efac6084d1f036ea5d60e5fa16fe
Marcelo Tournier <15373278+marcelotournier@users.noreply.github.com>
2022-02-27 18:38:06 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='jobmanager',
+    version='0.1',
+    description='simple job manager to run tasks in concurrency',
+    url='http://github.com/marcelotournier/jobmanager',
+    author='Marcelo Tournier',
+    author_email='marcelo@inova.life',
+    license='Apache 2.0',
+    packages=['jobmanager'],
+    zip_safe=False)
